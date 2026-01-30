d3fa717c7950bd522c0566c3ef2d6ccd565c7201
Brayden Youngberg <bjyberg1@gmail.com>
2026-01-30 08:51:14 +0100

    feat(mega commit): Finishing rough notebook prototype

--- helpers/data.js
+++ helpers/data.js
@@ -1,28 +1,21 @@
-  /**
-   * Add a cache-busting query string to URLs when running on Windows,
-   * to prevent TopoJSON from being loaded from cache.
-   *
-   * https://github.com/duckdb/duckdb-wasm/issues/1658
-   *
-   * @param {string} url - URL to patch
-   * @returns {string} patched URL if on Windows, original URL otherwise
-   */
+/**
+ * Add a cache-busting query string to URLs when running on Windows,
+ * to prevent TopoJSON from being loaded from cache.
+ *
+ * https://github.com/duckdb/duckdb-wasm/issues/1658
+ *
+ * @param {string} url - URL to patch
+ * @returns {string} patched URL if on Windows, original URL otherwise
+ */
 export function patchWindowsCache(url) {
   let isWindows = false;
-  if (navigator.userAgentData) { // not supported by Fx, although issue seems isolated to Chromium browsers
+  if (navigator.userAgentData) {
+    // not supported by Fx, although issue seems isolated to Chromium browsers
     isWindows = navigator.userAgentData.platform === "Windows";
   } else {
     isWindows = navigator.userAgent.toLowerCase().includes("windows");
   }
 
-  return isWindows
-    ? `${url}?t=${Date.now()}`
-    : url;
+  return isWindows ? `${url}?t=${Date.now()}` : url;
 }
 
-
-export const boundary_paths = {
-  admin0_path: "/data/shared/atlas_gaul_a0_africa_simple-vlowres.topojson", // lyr: atlas_gaul_a0_africa
-  admin1_path: "/data/shared/atlas_gaul_a1_africa_simple-vlowres.topojson", // lyr: atlas_gaul_a1_africa
-  admin2_path: "/data/shared/atlas_gaul_a2_africa_simple-lowres.topojson"   // lyr: atlas_gaul_a2_africa
-};
